942d7229f6218a68ac9855d9f644e8609d4d61e7
Lorenzo Hess <lhess@bowdoin.edu>
2022-11-19 18:38:18 -0500

    quality of life edits

--- latticeLayers.m
+++ latticeLayers.m
@@ -2,28 +2,48 @@
 i  = 7.0711 * 10^(-6); % i
 
 % Make 4 Layer objects
-Layer1 = Layer(8,4);
-Layer2 = Layer(8,4);
-Layer3 = Layer(8,4);
-Layer4 = Layer(8,4);
+Layer1 = Layer(4,2);
+Layer2 = Layer(4,2);
+Layer3 = Layer(4,2);
+Layer4 = Layer(4,2);
 
-% Put Layers in array to loop over
+% Put Layers initialXCoord array to loop over
 layersArray = [Layer1; Layer2; Layer3; Layer4];
 
-initialPhotonCoordsArray = [];
-initialXCoord = -i;
+negativeXCoord = -1*i; % initial coordinate of leftmost photon
+initialXCoord = -1*i;  % initial coordinate of rightmost photon
 initialYCoord = Layer1.lattice.lattice_width;
-numPhotons = (2*abs(initialXCoord))/(Layer1.lattice.general_photon_step*10);
+initialCoordsStepMultiplier = 100; % multiplies x-distance between each initial incident photon
+numPhotons = floor((2*abs(initialXCoord))/(Layer1.lattice.general_photon_step*initialCoordsStepMultiplier));
 disp("Num photons = " + numPhotons)
-while (initialXCoord < i)
+
+initialPhotonCoordsArray = [];
+while (initialXCoord < abs(negativeXCoord))
     initialPhotonCoordsArray = [initialPhotonCoordsArray; initialXCoord, initialYCoord];
-    initialXCoord = initialXCoord + (Layer1.lattice.general_photon_step*10);
+    initialXCoord = initialXCoord + (Layer1.lattice.general_photon_step*initialCoordsStepMultiplier);
 end
 
 finalPhotonCoordsArray = loopLayers(layersArray, initialPhotonCoordsArray);
+for idx = 1:size(layersArray)
+    layer = layersArray(idx);
+    disp(">>> Boundary Summary for Layer " + idx)
+    layer.lattice.boundarySummary();
+end
+
 Layer4.plotAll();
-plotPoints(finalPhotonCoordsArray,'r.');
-plotPoints(initialPhotonCoordsArray,'k.')
+plotPoints(finalPhotonCoordsArray,'r.'); % final inner bound in red
+plotPoints(initialPhotonCoordsArray,'k.') % initial coords in black
+absoluteFilePath = "/home/user/ffr-photon-simulator/Layers-images/"; % leave empty for current directory
+%saved = saveFigure(layersArray, Layer1.lattice.number_fibers_in_length, Layer1.lattice.number_fibers_in_width);
+
+% Save the plot as an image
+function bool = saveFigure(layersArray, layer1Length, layer1Width, absoluteFilePath)
+  frame = getframe(gcf);
+  figureName = absoluteFilePath + size(layersArray) + "__" + layer1Length + "x" + layer1Width + "_layers.png";
+  disp("### Figure saved as: " + figureName + "###")
+  imwrite(frame.cdata, figureName)
+  bool = true;
+end
 
 % Loop over Layers recursively
 function finalPhotonCoordsArray = loopLayers(layersArray, initialPhotonCoordsArray)
@@ -36,9 +56,6 @@ function finalPhotonCoordsArray = loopLayers(layersArray, initialPhotonCoordsArr
         disp("=========Ray traced layer=========")
     end
 end
-
-% Display the photons that reached the inner bound
-%disp(finalPhotonCoordsArray)
 
 function array = makeNextInitialCoordsArray(layer, bottomBoundCoordArray)
     % Add the width of the lattice (positive y-value) to each y-value in
