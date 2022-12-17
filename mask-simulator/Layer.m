7afcbf3b09cd55815c0bb4e67e322d11215ee3c7
Lorenzo Hess <lhess@bowdoin.edu>
2022-12-16 22:58:18 -0500

    Add comments

--- mask-simulator/Layer.m
+++ mask-simulator/Layer.m
@@ -24,22 +24,27 @@ classdef Layer
       fiberData = obj.lattice.bb_data;
     end
 
+    % Get the axis handle
     function axisHandle = getAxisHandle(obj)
       axisHandle = obj.lattice.bb_axisHandle;
     end
 
+    % Get the inner bound y coord value
     function innerBound = getInnerBound(obj)
       innerBound = obj.lattice.innerBound;
     end
 
+    % Get the outer bound y coord value
     function outerBound = getOuterBound(obj)
       outerBound = obj.lattice.outerBound;
     end
 
+    % Get the left bound x coord value
     function leftBound = getLeftBound(obj)
       leftBound = obj.lattice.leftBound;
     end
 
+    % Get the right bound x coord value
     function rightBound = getRightBound(obj)
       rightBound = obj.lattice.rightBound;
     end
