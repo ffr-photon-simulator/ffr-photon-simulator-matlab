3fe0d0bd5942b84ff4086c0768527b4036d16bf0
Lorenzo Hess <lhess@bowdoin.edu>
2023-02-12 22:31:36 -0500

    Build Quadrant configs

--- mask-simulator/config.m
+++ mask-simulator/config.m
@@ -29,3 +29,21 @@
 ffrConfig = struct();
 ffrConfig = structInputOrDefault("How many FFR layers?", ffrConfig, 'nLayers', Defaults.nLayers);
 
+    % Iterate over each quadrant and build its config
+    for q = 1:qls.nQuadrants
+      disp("")
+      disp("### Configure quadrant " + q + ".")
+      qsv1 = struct();
+      qsv1 = structInputOrDefault("Length?", qsv1, 'length', Defaults.qLength);
+      qsv1 = structInputOrDefault("Min fiber radius?", qsv1, 'minRadius', Defaults.minRadius);
+      qsv1 = structInputOrDefault("Max fiber radius?", qsv1, 'maxRadius', Defaults.maxRadius);
+      qsv1 = structInputOrDefault("Density?", qsv1, 'density', Defaults.density);
+      qsv1.width = qls.width;
+      qsv1.frameSize = [qsv1.length, qsv1.width];
+      qsv1.circSize = [qsv1.minRadius, qsv1.maxRadius];
+
+      % Add quadrant length to the quadrant layer length.
+      qls.length = qls.length + qsv1.length;
+      % Add the quadrant struct to the list
+      quadrantConfigsv1 = [quadrantConfigsv1; qsv1];
+    end
