9229ce5de73960c6cfc0194c0c6c01fb6a846d62
Lorenzo Hess <lhess@bowdoin.edu>
2023-02-12 22:16:35 -0500

    Add proper constructor to work with QuadrantLayer

--- mask-simulator/Quadrant.m
+++ mask-simulator/Quadrant.m
@@ -1,14 +1,22 @@
 classdef Quadrant
-  % A data class representing a quadrant in the lattice.
+  % A data class representing a quadrant in a quadrant layer.
   % Variable names are copied from bubblebath_noPlot
   % when possible to make the connection clear.
   %
   % The constructor runs bubblebath_noPlot to get the
   % fiber data (also receives the config struct).
+  properties (Constant)
+    F_MIN_SEPARATION = 2*10^(-6);
+
+    LATTICE_I = 10*10^(-6); % convenient x-axis basis for lattice length
+    LATTICE_J = 10*10^(-6); % convenient y-axis basis for lattice width
+  end
 
   properties
     minRadius
     maxRadius
+    density
+    frameSize % quadrant size
 
     bb_struct = struct();
     bb_struct_out % the struct returned from bubblebath_noPlot
@@ -20,12 +28,19 @@ classdef Quadrant
   end
 
   methods
-    function obj = Quadrant(frameSize, minRadius, maxRadius, density)
-      obj.minRadius = minRadius;
-      obj.maxRadius = maxRadius;
+    function obj = Quadrant(config)
+      % The config 'c' is a struct of the form:
+      % c.minRadius = minimum fiber radius
+      % c.maxRadius = maximum fiber radius
+      % c.frameSize = quadrant size
+      % c.density   = fiber density
 
-      obj.bb_struct.frameSize      = frameSize; % [length, width], centered at [0,0].
-      obj.bb_struct.circSize       = [minRadius, maxRadius];
+      % Store the user-defined values
+      obj.minRadius = config.minRadius;
+      obj.maxRadius = config.maxRadius;
+      obj.frameSize = config.frameSize;
+      obj.density   = config.density;
+
       obj.bb_struct.nSizes         = 10; % number of discrete radii in interval
       obj.bb_struct.maxCircsPerRad = 500;
       obj.bb_struct.maxIt          = 200;
@@ -36,6 +51,12 @@ classdef Quadrant
       obj.bb_struct.drawFrame      = true;
 
       [obj.bb_data, obj.bb_struct_out] = bubblebath_noPlot(obj.bb_struct);
+    function data = addLengthOffset(obj, offset)
+      % Add a length offset to this quadrant
+      data = obj.bb_data;
+      data(:,1) = data(:,1) + offset;
+    end
+
     end
   end
 end
