61e9b12bdd831e334a166bb64780b0af90d02e1c
Lorenzo Hess <lhess@bowdoin.edu>
2023-02-12 22:17:12 -0500

    Clarify bubblebath_noPlot usage

--- mask-simulator/Quadrant.m
+++ mask-simulator/Quadrant.m
@@ -19,12 +19,10 @@ classdef Quadrant
     frameSize % quadrant size
 
     bb_struct = struct();
+
+    % Output of bubblebath_noPlot()
     bb_struct_out % the struct returned from bubblebath_noPlot
-
-    bb_frameSize
-    bb_density
-
-    bb_data
+    bb_data % ([xcoord ycoord radius])
   end
 
   methods
@@ -41,22 +39,38 @@ classdef Quadrant
       obj.frameSize = config.frameSize;
       obj.density   = config.density;
 
+      % Set other values necessary for bubblebath_noPlot().
+      obj.bb_struct.frameSize      = obj.frameSize; % [length, width], centered at [0,0].
+      obj.bb_struct.circSize       = [obj.minRadius, obj.maxRadius];
       obj.bb_struct.nSizes         = 10; % number of discrete radii in interval
       obj.bb_struct.maxCircsPerRad = 500;
       obj.bb_struct.maxIt          = 200;
       obj.bb_struct.edgeType       = 1; % all inside frame
-      obj.bb_struct.density        = density; % not yet sure what the "density" does in bubblebath.m
-      obj.bb_struct.overlap        = QuadrantLattice.F_MIN_SEPARATION;
+      obj.bb_struct.density        = obj.density; % not yet sure what the "density" does in bubblebath.m
+      obj.bb_struct.overlap        = obj.F_MIN_SEPARATION;
       obj.bb_struct.overlapType    = 'absolute';
       obj.bb_struct.drawFrame      = true;
 
+      % Run bubblebath_noPlot() and store the output.
       [obj.bb_data, obj.bb_struct_out] = bubblebath_noPlot(obj.bb_struct);
+
+      % Add the length offset to the x-values in bb_data and store the data.
+      disp(config)
+      obj.bb_data = obj.addLengthOffset(config.lengthOffset);
+      %disp(obj.bb_data)
+      %disp("Quadrant data size")
+      %disp(size(obj.bb_data))
+    end
+
     function data = addLengthOffset(obj, offset)
       % Add a length offset to this quadrant
       data = obj.bb_data;
       data(:,1) = data(:,1) + offset;
     end
 
+    % GETTERS
+    function data = getFiberData(obj)
+      data = obj.bb_data;
     end
   end
 end
