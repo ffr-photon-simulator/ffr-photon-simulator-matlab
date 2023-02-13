0fbb0be6efcec9f7af4d3f3a32bfa820feeaaffa
Lorenzo Hess <lhess@bowdoin.edu>
2023-02-12 22:29:16 -0500

    Show outline of FFR config struct

---
+++ mask-simulator/config.m
@@ -0,0 +1,27 @@
+%%% Configuration file for the simulator.
+% The FFR is configured from FFR -> FFR layers -> quadrant layers -> quadrants.
+% You must edit this config file to configure the FFR, and functions will
+% prompt you for the remaining configuration parameters. There will always be
+% a default option.
+
+% FFR Config (struct)
+%   ffrConfig.nLayers
+%   ffrConfig.ffrLayerConfigs (list of structs 'ls')
+%     ls.nQLayers
+%     ls.layerType
+%     ls.qLayerConfigs (list of structs 'qls')
+%       qls.nQuadrants
+%       qls.width
+%       qls.length
+%       qls.heightOffset
+%       qls.quadrantConfigs (list of structs 'qs')
+%         qs.minRadius
+%         qs.maxRadius
+%         qs.density
+%         qs.length
+%         qs.lengthOffset
+%         qs.width = qls.width
+%         qs.frameSize = [length, width]
+%         qs.circSize = [minRadius, maxRadius]
+%
+
