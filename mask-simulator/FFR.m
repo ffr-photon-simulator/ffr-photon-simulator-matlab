4531e3b29414d8911453aa615cf3ce8a93906475
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-06 00:29:20 -0500

    Instantiate Boundaries in the constructor

--- mask-simulator/FFR.m
+++ mask-simulator/FFR.m
@@ -2,6 +2,9 @@ classdef FFR
   % A class representing an FFR.
   properties
     ffrLayers = [];
+    boundaries = struct();
+    ffrBounds = [];
+    fiberData = [];
   end
 
   methods
@@ -20,6 +23,38 @@ classdef FFR
         ffrLayer = FFRLayer(ffrLayerConfigs(l));
         obj.ffrLayers = [obj.ffrLayers; ffrLayer];
       end
+
+      % Create Boundaries
+      % FFR bounds
+      ffrBounds = struct();
+      ffrBounds.leftBound = FFRBoundary(config.boundaries.ffrBounds.leftBound, 'left');
+      ffrBounds.rightBound = FFRBoundary(config.boundaries.ffrBounds.rightBound, 'right');
+      ffrBounds.innerBound = FFRBoundary(config.boundaries.ffrBounds.innerBound, 'inner');
+      ffrBounds.outerBound = FFRBoundary(config.boundaries.ffrBounds.outerBound, 'outer');
+      obj.ffrBounds = ffrBounds;
+
+      % Interior bounds
+      interiorBounds = [];
+      for i = 1:size(config.boundaries.interiorBounds)
+        interiorBounds = [interiorBounds; InteriorBoundary(config.boundaries.interiorBounds(i))];
+      end
+      Defaults.debugMessage("Interior bounds in FFR constructor", 1);
+      obj.boundaries.ffrBounds = ffrBounds;
+      obj.boundaries.interiorBounds = interiorBounds;
+
+      % Aggregate entire FFR fiber data
+      obj.fiberData = obj.buildFiberData();
+    end
+
+
+    function bounds = printBounds(obj)
+      bounds = "FFR Bounds:\n-> Left: " + string(obj.ffrBounds.leftBound.bound);
+      %disp("FFR Bounds:")
+      %disp("-> Left: "  + obj.ffrBounds.leftBound.bound)
+      %disp("-> Right: " + obj.ffrBounds.rightBound.bound)
+      %disp("-> Outer: " + obj.ffrBounds.outerBound.bound)
+      %disp("-> Inner: " + obj.ffrBounds.innerBound.bound)
+      %disp("")
     end
   end
 end
