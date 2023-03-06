ff6e27b1fb9dcc0d7c0648cacdf7043e0fe7aea5
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-05 23:59:20 -0500

    Variable renaming and other basic edits

--- mask-simulator/Defaults.m
+++ mask-simulator/Defaults.m
@@ -4,21 +4,26 @@ classdef Defaults
     %%% INITIAL PHOTONS
     % Multipliers
     % Change the x-separation between each initial photon:
-    %  >1 decreases the number of photons
-    %  <1 increases the number of photons
+    %  >1 increases the number of photons
+    %  <1 decreases the number of photons
     separationMultiplier = 100;
     % Change the length of the initial photons' y step:
     %  >1 increases the y step
     %  <1 decreases the y step
     yStepMultiplier = 1;
+    % Wavelength
+    photonWavelength = 2.5 * 10^(-7);
+    % Separation
+    initialSeparation = Defaults.photonWavelength / 2 * Defaults.separationMultiplier;
 
     % Steps
     initialXStep = 0;
     % Define two initial y steps, one for ray tracing from the outer layer to
     % the inner layer of the FFR, the other for the opposite direction. This
     % is the only change necessary to ray trace in the opposite direction.
-    %outerToInnerYStep = -(Photon.WAVELENGTH/2)*yStepMultiplier;
-    %innerToOuterYStep = -outerToInnerYStep;
+    outerToInnerYStep = -(Defaults.photonWavelength/2)*Defaults.yStepMultiplier;
+    %outerToInnerYStep = -1.25 * 10^(-7);
+    innerToOuterYStep = -Defaults.outerToInnerYStep;
     % The x-separation between each initial photon.
     %separation = abs(outerToInnerYStep)*separationMultiplier;
 
@@ -28,15 +33,16 @@ classdef Defaults
 
     LATTICE_I = 10*10^(-6); % convenient x-axis basis for lattice length
     LATTICE_J = 10*10^(-6); % convenient y-axis basis for lattice width
+
     % FFR Config
-    nLayers = 3;
+    nLayers = 1;
 
-    % FFR Layers Config
+    % FFR Layers
     nQLayers = 1;
-    layerType = 'filtering'; % inner, filterting, outer
+    layerType = 'filtering'; % inner, filterting, outer %%% <--
 
-    % Quadrant layer config
-    nQuadrants = 2;
+    % Quadrant layers
+    nQuadrants = 8;
     qlWidth = 4*Defaults.LATTICE_J;
     %qlLength = 8 * LATTICE_I;
 
