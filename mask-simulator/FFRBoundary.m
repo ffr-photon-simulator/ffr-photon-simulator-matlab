4bbdc45457d7ef509fdd41b0155e8fbee5e3ad02
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-06 00:15:46 -0500

    Add the FFR Boundary object
    The FFRBoundary inherits from Boundary. Its only additional
    property is its "type", which is either: left, right, outer, inner.
    Calling the Boundary constructor is easy.

---
+++ mask-simulator/FFRBoundary.m
@@ -0,0 +1,38 @@
+classdef FFRBoundary < Boundary
+  % Represents the exterior boundaries of the FFR.
+  % Keeps track of how many photons have crossed the boundary.
+  % Exterior boundaries are: left, right, outer, inner.
+  % Store the count data and put it in a struct  when the user requests it.
+
+  properties
+    type % left, right, outer, or inner
+  end
+
+  methods
+    function obj = FFRBoundary(bound, type)
+      % TODO Verify constructors work this way (assign value to superclass properties)
+      obj = obj@Boundary(bound);
+      obj.type = type;
+    end
+
+
+    function addCrossing(obj, photon)
+      % Increment the following counts:
+      %  - total count
+      obj.increment(obj.count);
+    end
+
+    function plot(obj, axisHandle)
+      if isequal(obj.type, 'inner') || isequal(obj.type, 'outer')
+        yline(axisHandle, obj.bound, Defaults.ffrBoundStyle, obj.bound, 'LineWidth', Defaults.ffrBoundWeight);
+      else
+        xline(axisHandle, obj.bound, Defaults.ffrBoundStyle, obj.bound, 'LineWidth', Defaults.ffrBoundWeight);
+      end
+    end
+
+    function data = getCountData(obj)
+      data = struct();
+      data.count = obj.count;
+    end
+  end
+end
