a04aa9669b193b85e4fba91bc925d03463180dba
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-06 00:47:13 -0500

    Add makeInitialPhotons()
    Use the y value of the outer boundary as the initial y value of
    the Photons.

--- mask-simulator/simulator.m
+++ mask-simulator/simulator.m
@@ -6,5 +6,26 @@ config
 ffr = FFR(ffrConfig);
 
 
+% Generate the incoming photons
+xStart = -Defaults.nQuadrants * Defaults.qLength / 2;
+xEnd = -xStart;
+outerBound = ffr.ffrBounds.outerBound;
+initialPhotons = makeInitialPhotons(xStart, xEnd, Defaults.initialSeparation, outerBound, Defaults.initialXStep, Defaults.outerToInnerYStep);
 
-return;
+
+
+% FUNCTIONS
+function photons = makeInitialPhotons(xStart, xEnd, separation, outerBoundary, initialXStep, outerToInnerYStep)
+  % The photons' x-axis range is from xStart to xEnd. Their y coordinate is
+  % equal to the value of the outer boundary. The initial photons are
+  % separated by the value of the "separation" variable.
+  nPhotons = (xEnd-xStart) / separation;
+  disp("Num photons: " + nPhotons)
+  photons = [];
+  bound = outerBoundary.bound;
+  for m = 1:nPhotons
+    nextPhoton = Photon(xStart + m*separation, bound, initialXStep, outerToInnerYStep);
+    %nextPhoton = Photon(xStart + m*separation, Defaults.nQLayers * Defaults.qlWidth, initialXStep, outerToInnerYStep);
+    photons = [photons; nextPhoton];
+  end
+end
