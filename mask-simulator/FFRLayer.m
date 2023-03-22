a84ba878e095f70c5be84f0af73e17f5b3d9a56c
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-22 16:41:30 -0400

    Deletions

--- mask-simulator/FFRLayer.m
+++ mask-simulator/FFRLayer.m
@@ -22,31 +22,15 @@ classdef FFRLayer
       % Store list of structs which are the configs of the quadrant layers.
       % Each quadrant layer struct holds the data to make that quadrant layer.
       qLayerConfigs = config.quadrantLayerConfigs; % [struct1, struct2, struct3, ...]
-      %disp("> ffr layer constructor qlayer configs")
-      %disp(qLayerConfigs)
 
       % Create QuadrantLayers
       for q = 1:config.nQLayers
-        % if q == 3
-        %   disp("")
-        %   disp("> Create quadrant layer " + q)
-        % end
         quadrantLayer = QuadrantLayer(qLayerConfigs(q));
-        % if q == 3
-        %   disp("> Quadrant layer 3 data")
-        %   disp(quadrantLayer.getFiberData())
-        % end
         obj.quadrantLayers = [obj.quadrantLayers; quadrantLayer];
       end
 
-      %disp("> ffrlayer constructor qlayers")
-      %disp(obj.quadrantLayers)
-
       % Aggregate fiber data from the quadrant layers
       obj.lattice = obj.makeLattice(config.nQLayers);
-      %disp("FFR Layer lattice")
-      %disp(obj.lattice)
-      %disp("")
     end
 
     function lattice = makeLattice(obj, nLayers)
@@ -54,58 +38,9 @@ classdef FFRLayer
       lattice = [];
       for n = 1:nLayers
         lattice = [lattice; obj.quadrantLayers(n).getFiberData()];
-        %disp("Quadrant layer " + n + " data:")
-        %disp(obj.quadrantLayers(n).getFiberData())
       end
     end
 
-    %function obj = Layer(length, width, minRadius, maxRadius, density, optional_fiberData)
-    %  obj.latticeLength = length*BubblebathFiberLattice.LATTICE_I;
-    %  obj.latticeWidth = width*BubblebathFiberLattice.LATTICE_J;
-    %  frameSize = [obj.latticeLength, obj.latticeWidth];
-    %  minRadius = 4*10^(-6);
-    %  maxRadius = 4.01*10^(-6);
-    %  density = 0.05;
-    %  obj.lattice = BubblebathFiberLattice(frameSize, minRadius, maxRadius, density, optional_fiberData);
-    %end
-
-    % Plotting
-    %function plotLayer(obj, figure)
-    %  % Plot this layer on a given figure.
-    %end
-
-    %% GETTERS
-    %% Any attribute of the lattice should be accessed with a getter.
-    %function fiberData = getFiberData(obj)
-    %  % Return the bb_data of the lattice.
-    %  % fiberData is [xcoord, ycoord, radius].
-    %  fiberData = obj.lattice.bb_data;
-    %end
-
-    %% Get the axis handle
-    %function axisHandle = getAxisHandle(obj)
-    %  axisHandle = obj.lattice.bb_axisHandle;
-    %end
-
-    %% Get the inner bound y coord value
-    %function innerBound = getInnerBound(obj)
-    %  innerBound = obj.lattice.innerBound;
-    %end
-
-    %% Get the outer bound y coord value
-    %function outerBound = getOuterBound(obj)
-    %  outerBound = obj.lattice.outerBound;
-    %end
-
-    %% Get the left bound x coord value
-    %function leftBound = getLeftBound(obj)
-    %  leftBound = obj.lattice.leftBound;
-    %end
-
-    %% Get the right bound x coord value
-    %function rightBound = getRightBound(obj)
-    %  rightBound = obj.lattice.rightBound;
-    %end
     function bool = containsPhoton(obj, photon)
       % Check if a photon is inside this layer.
       bool = false;
