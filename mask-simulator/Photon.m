5ea1c9e4e261251f338ea86198a606fffa3c288b
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-23 19:44:29 -0400

    Deletions and comments

--- mask-simulator/Photon.m
+++ mask-simulator/Photon.m
@@ -23,49 +23,27 @@ classdef Photon < handle
 
     function crossed = hasCrossedFFRBound(obj, bound)
       if isequal(bound.type, 'left') && obj.x <= bound.bound
-        %disp('Crossed FFR bound: left')
         crossed = true;
       elseif isequal(bound.type, 'right') && obj.x >= bound.bound
-        %disp('Crossed FFR bound: right')
         crossed = true;
       elseif isequal(bound.type, 'outer') && obj.y >= bound.bound
-        %disp('Crossed FFR bound: outer')
         crossed = true;
       elseif isequal(bound.type, 'inner') && obj.y <= bound.bound
-        %disp('Crossed FFR bound: inner')
         crossed = true;
       else
-        %disp('NOT FFR BOUND')
         crossed = false;
       end
     end
-
-    %function crossed = hasCrossedInteriorBound(obj, bound)
-    %  % If the photon is in an Interior bound's range (between its upper and lower bounds),
-    %  % then it has crossed into the boundary.
-    %  crossed = false;
-    %  if obj.y <= bound.upperBound && obj.y >= bound.lowerBound
-    %    if obj.insideInteriorBound == false
-    %      crossed = true;
-    %    end
-    %  else
-    %    obj.insideInteriorBound == true;
-    %  end
-    %end
-
     
     function move(obj)
       obj.x = obj.x + obj.xStep;
       obj.y = obj.y + obj.yStep;
     end
 
-    function obj = moveX(obj)
-      obj.x = obj.x + obj.xStep;
-    end
-
-    function obj = moveY(obj)
-      obj.y = obj.y + obj.yStep;
-    end
+    %function obj = move(obj)
+    %  obj.x = obj.x + obj.xStep;
+    %  obj.y = obj.y + obj.yStep;
+    %end
 
     function coords = getCoords(obj)
       coords = [obj.x obj.y];
@@ -76,21 +54,9 @@ classdef Photon < handle
       obj.yStep = newYStep;
     end
 
-    function obj = setY(obj, newY)
-      obj.y = newY;
-    end
-
-    function obj = setSteps(obj, xStep, yStep)
-      obj.xStep = xStep;
-      obj.yStep = yStep;
-    end
-
-    function obj = setXStep(obj, step)
-      obj.xStep = step;
-    end
-
-    function obj = setYStep(obj, step)
-      obj.yStep = step;
-    end
+    %function obj = setSteps(obj, xStep, yStep)
+    %  obj.xStep = xStep;
+    %  obj.yStep = yStep;
+    %end
   end
 end
