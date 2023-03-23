2c3fe3400225f604f5be44da1a9a9e6563d9d230
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-23 17:55:35 -0400

    Add nLayers property and remove old FFRLayer creator

--- mask-simulator/FFR.m
+++ mask-simulator/FFR.m
@@ -1,6 +1,7 @@
 classdef FFR
   % A class representing an FFR.
   properties
+    nLayers
     ffrLayers = [];
     boundaries = struct();
     ffrBounds = [];
@@ -13,16 +14,7 @@ classdef FFR
       % c.nLayers = number of layers in FFR
       % c.ffrLayerConfigs = list of structs defining each FFRLayer
 
-      % Store list of structs which are the configs of the FFRLayers.
-      % Each FFRLayer struct holds the data to make that FFR layer.
-      ffrLayerConfigs = config.ffrLayerConfigs;
-
-      % Create FFRLayers
-      for l = 1:config.nLayers
-        %disp("Create FFRLayer " + q)
-        ffrLayer = FFRLayer(ffrLayerConfigs(l));
-        obj.ffrLayers = [obj.ffrLayers; ffrLayer];
-      end
+      obj.nLayers = config.nLayers;
 
       % Create Boundaries
       % FFR bounds
