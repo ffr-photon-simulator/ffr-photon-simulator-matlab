680974c215edfaabe7076ab450ace263aa4cc2ae
Lorenzo Hess <lhess@bowdoin.edu>
2023-03-26 20:22:51 -0400

    Preallocate, vectorize, and add relevant properties
    1. Preallocate the Quadrant object array
    2. Preallocate QuadrantLayer fiberData lattice
    3. Vectorize the sum to get nFibers

--- mask-simulator/QuadrantLayer.m
+++ mask-simulator/QuadrantLayer.m
@@ -1,12 +1,14 @@
 classdef QuadrantLayer
   % Stores fiber data representing several quadrants of variable fiber density or radial range.
   properties
-    quadrants = [];
+    quadrants
+    nQuadrants
     lattice = [];
     qlWidth
     qlLength
     outerBound
     innerBound
+    nFibers
   end
 
   methods
@@ -20,23 +22,30 @@ classdef QuadrantLayer
       
       % Store list of structs which are the configs of the actual quadrants.
       quadrantConfigs = config.quadrantConfigs; % [qlist1, qlist2, qlist3, ...]
+      obj.nQuadrants = config.nQuadrants;
 
       % Create quadrants
-      for q = 1:config.nQuadrants
-        quadrant = Quadrant(quadrantConfigs(q));
-        obj.quadrants = [obj.quadrants; quadrant];
+      % Preallocate the array and create the last Quadrant in the array.
+      quadrants(1, single(obj.nQuadrants)) = Quadrant(quadrantConfigs(end));
+      for q = 1:single(obj.nQuadrants - 1)
+        quadrants(q) = Quadrant(quadrantConfigs(q));
       end
+      % Transpose the row vector to avoid rewriting other code.
+      obj.quadrants = quadrants.';
 
-      % Sum quadrant lengths and widths
+      % Set quadrant layer length and width
       obj.qlLength = config.length;
       obj.qlWidth = config.width;
+
+      % Sum number of fibers
+      obj.nFibers = sum([obj.quadrants.nFibers]);
 
       % Bound values
       obj.innerBound = config.heightOffset;
       obj.outerBound = obj.innerBound + obj.qlWidth;
 
       % Aggregate quadrant fiber data
-      obj.lattice = obj.makeLattice(config.nQuadrants);
+      obj.lattice = obj.makeLattice(obj.nQuadrants);
 
       % Adjust y-values of the fibers to account for:
       %  - bubbblebath_noPlot() centers around [0,0]  -> add half this quadrant layer's width
@@ -52,9 +61,28 @@ classdef QuadrantLayer
 
     function lattice = makeLattice(obj, nQuadrants)
       % Add the fiber data from each quadrant to this quadrant layer.
-      lattice = [];
-      for n = 1:nQuadrants
-        lattice = [lattice; obj.quadrants(n).getFiberData()];
+      lattice = nan(obj.nFibers, 3);
+      for qNum = 1:nQuadrants
+        fiberData = obj.quadrants(qNum).getFiberData();
+        % The function which maps a fiber in a quadrant defined
+        % by its index in the quarant qIdx and the index in the
+        % iteration through all the quadrants qNum is:
+        %
+        % latticeIdx(qIdx, qNum) = nQFibers*(qNum - 1) + qIdx;
+        %
+        % For example, assume nQFibers = 5 and there are 5 quadrants in total:
+        % latticeIdx(1, 1)       = 5*(1-1) + 1 = 1;
+        % latticeIdx(1, 2)       = 5*(2-1) + 1 = 6;
+        % latticeIdx(2, 2)       = 5*(2-1) + 2 = 7;
+        % latticeIdx(5, 4)       = 5*(4-1) + 5 = 20;
+        % latticeIdx(5, 5)       = 5*(5-1) + 5 = 25;
+
+        % Get the number of fibers in the current quadrant.
+        nQFibers = size(fiberData, 1);
+        for qIdx = 1:nQFibers
+          latticeIdx = nQFibers*(qNum - 1) + qIdx;
+          lattice(latticeIdx,:,:) = fiberData(qIdx,:,:);
+        end
       end
     end
 
