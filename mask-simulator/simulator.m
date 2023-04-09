fca259b7f3f0ec703330b2ab7d044a1aa3c854eb
lhess2021 <lorenzohess@tutanota.com>
2023-04-08 23:54:44 -0400

    Use LaTeX interpreter for text on figures

--- mask-simulator/simulator.m
+++ mask-simulator/simulator.m
@@ -1,4 +1,11 @@
 plotbool = true;
+
+list_factory = fieldnames(get(groot, 'factory'));
+index_interpreter = find(contains(list_factory, 'Interpreter'));
+for i = 1:length(index_interpreter)
+  default_name = strrep(list_factory{index_interpreter(i)}, 'factory', 'default');
+  set(groot, default_name, 'latex');
+end
 %clear all; % clear workspace
 
 %cwd = pwd % current working directory
